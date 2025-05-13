1b17b6ec7e8031f07539d10807628ea1312d9455
HankTk <hidenori@comcast.net>
2025-05-13 12:35:19 -0700

    Initial Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'bpmn-js-angular';
+  diagramUrl = 'assets/diagrams/diagram.bpmn';
+  importError?: Error;
+
+  handleImported(event) {
+    const {
+      type,
+      error,
+      warnings
+    } = event;
+
+    if (type === 'success') {
+      console.log(`Rendered diagram (%s warnings)`, warnings.length);
+    }
+
+    if (type === 'error') {
+      console.error('Failed to render diagram', error);
+    }
+
+    this.importError = error;
+  }
+}
